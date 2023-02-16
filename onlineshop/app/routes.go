704cfc854bb1dcfb2ad50d65bd6758ed67fbf0b1
zhanarrakhmetova <82767082+rakhmetovazhanar@users.noreply.github.com>
2023-02-16 16:47:32 +0600

    Delete onlineshop directory

--- onlineshop/app/routes.go
+++
@@ -1,9 +0,0 @@
-package app
-
-import (
-	"github.com/Aktollkynn/GoProject.git/app/controllers"
-)
-
-func (server *Server) InitializeRoutes() {
-	server.Router.HandleFunc("/", controllers.Home).Methods("GET")
-}
--- onlineshop/main.go
+++
@@ -1,7 +0,0 @@
-package main
-
-import "github.com/Aktollkynn/GoProject.git/app"
-
-func main() {
-	app.Run()
-}
