6bd108e0b41b40048892b976192376cc4808bd67
Aktollkynn <91051893+Aktollkynn@users.noreply.github.com>
2023-02-16 16:37:37 +0600

    pro2

---
+++ app/registry.go
@@ -0,0 +1,14 @@
+package app
+
+import "github.com/Aktollkynn/GoProject.git/app/models"
+
+type Model struct {
+	Model interface{}
+}
+
+func RegisterModels() []Model {
+	return []Model{
+		{Model: models.User{}},
+		{Model: models.Address{}},
+	}
+}
---
+++ app/server.go
@@ -0,0 +1,95 @@
+package app
+
+import (
+	"fmt"
+	"github.com/joho/godotenv"
+	"gorm.io/driver/postgres"
+	"log"
+	"net/http"
+	"os"
+
+	"github.com/gorilla/mux"
+	"gorm.io/gorm"
+)
+
+type Server struct {
+	DB     *gorm.DB
+	Router *mux.Router
+}
+
+type AppConfig struct {
+	AppName string
+	AppEnv  string
+	AppPort string
+}
+
+type DBConfig struct {
+	DBHost     string
+	DBUser     string
+	DBPassword string
+	DBName     string
+	DBPort     string
+}
+
+func (server *Server) Initialize(appConfig AppConfig, dbconfig DBConfig) {
+	fmt.Println("Welcome to " + appConfig.AppName)
+
+	server.initializeDB(dbconfig)
+	server.InitializeRoutes()
+}
+
+func (server *Server) Run(addr string) {
+	fmt.Printf("Listening to port %s", addr)
+	log.Fatal(http.ListenAndServe(addr, server.Router))
+}
+
+func (server *Server) initializeDB(dbConfig DBConfig) {
+	var err error
+	dsn := fmt.Sprintf("host=%s user=%s password=%s dbname=%s port=%s sslmode=disable TimeZone=Asia/Jakarta", dbConfig.DBHost, dbConfig.DBUser, dbConfig.DBPassword, dbConfig.DBName, dbConfig.DBPort)
+	server.DB, err = gorm.Open(postgres.Open(dsn), &gorm.Config{})
+	if err != nil {
+		panic("Failed on connecting to the database server")
+	}
+
+	for _, model := range RegisterModels() {
+		err = server.DB.Debug().AutoMigrate(model.Model)
+
+		if err != nil {
+			log.Fatal(err)
+		}
+	}
+
+	fmt.Println("Database migrated successfully.")
+}
+
+func getEnv(key, fallback string) string {
+	if value, ok := os.LookupEnv(key); ok {
+		return value
+	}
+
+	return fallback
+}
+
+func Run() {
+	var server = Server{}
+	var appConfig = AppConfig{}
+	var dbConfig = DBConfig{}
+
+	err := godotenv.Load()
+	if err != nil {
+		log.Fatalf("Error on loading .env file")
+	}
+
+	appConfig.AppName = getEnv("APP_NAME", "TolkynSite")
+	appConfig.AppEnv = getEnv("APP_ENV", "development")
+	appConfig.AppPort = getEnv("APP_PORT", "9000")
+
+	dbConfig.DBHost = getEnv("DB_HOST", "localhost")
+	dbConfig.DBUser = getEnv("DB_USER", "postgres")
+	dbConfig.DBPassword = getEnv("DB_PASSWORD", "aktolkyn")
+	dbConfig.DBName = getEnv("DB_NAME", "shop")
+	dbConfig.DBPort = getEnv("DB_PORT", "5432")
+
+	server.Initialize(appConfig, dbConfig)
+	server.Run(":" + appConfig.AppPort)
+}
