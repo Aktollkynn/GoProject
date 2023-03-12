2cfce34934557faf58b33e9118f37b9555715a59
Galymzhan Tolepbergen <galymjan2002g@mail.ru>
2023-03-12 06:04:45 +0600

    Report-5 (register,login+logout)

--- app/controllers/home_controller.go
+++ app/controllers/home_controller.go
@@ -3,12 +3,18 @@ package controllers
 import (
 	"database/sql"
 	"fmt"
-	_ "github.com/lib/pq"
 	"html/template"
 	"net/http"
+
+	"github.com/gorilla/sessions"
+	_ "github.com/lib/pq"
 )
 
-func register(w http.ResponseWriter, r *http.Request) {
+var store = sessions.NewCookieStore([]byte("super-secret-key"))
+
+func Register(w http.ResponseWriter, r *http.Request) {
+	fmt.Println("***Register running***")
+
 	t, err := template.ParseFiles("templates/register.html")
 	if err != nil {
 		fmt.Fprintf(w, err.Error())
@@ -16,13 +22,15 @@ func register(w http.ResponseWriter, r *http.Request) {
 	t.Execute(w, "register")
 }
 
-func save_user(w http.ResponseWriter, r *http.Request) {
+func RegisterAuth(w http.ResponseWriter, r *http.Request) {
+	fmt.Println("***RegisterAuthHandler running***")
+
 	Fname := r.FormValue("first_name")
 	Lname := r.FormValue("last_name")
 	Email := r.FormValue("email")
 	Password := r.FormValue("password")
 
-	db, err := sql.Open("postgres", "postgresql://postgres:aktolkyn@localhost:5432/shop?sslmode=disable")
+	db, err := sql.Open("postgres", "postgresql://postgres:justice@localhost:5432/shop?sslmode=disable")
 	if err != nil {
 		http.Error(w, err.Error(), http.StatusInternalServerError)
 		return
@@ -35,26 +43,105 @@ func save_user(w http.ResponseWriter, r *http.Request) {
 		return
 	}
 	defer insert.Close()
-	http.Redirect(w, r, "/home_page", http.StatusSeeOther)
+	fmt.Printf("~~~New user altered :[first_mame: '%s', last_name:'%s', email:'%s', password: '%s']", Fname, Lname, Email, Password)
+
+	fmt.Println("==Successfully Registered==")
+	http.Redirect(w, r, "/login", http.StatusSeeOther)
 }
 func Login(w http.ResponseWriter, r *http.Request) {
+	fmt.Println("***Login running***")
 	t, err := template.ParseFiles("templates/login.html")
 	if err != nil {
 		fmt.Fprintf(w, err.Error())
 	}
 	t.Execute(w, "login")
 }
+
+// New 00:50
+var (
+	EmailNew    string
+	PasswordNew string
+)
+
+func LoginAuth(w http.ResponseWriter, r *http.Request) {
+	fmt.Println("***LoginAuthHandler running***")
+	r.ParseForm()
+	Email := r.FormValue("email")
+	Password := r.FormValue("password")
+	fmt.Println(":: Your input ->>  Email:", Email, "Password:", Password)
+	EmailNew = Email
+	PasswordNew = Password
+	db, err := sql.Open("postgres", "postgresql://postgres:justice@localhost:5432/shop?sslmode=disable")
+	if err != nil {
+		panic(err.Error())
+	}
+	defer db.Close()
+	//-----
+	rows, err := db.Query("SELECT email, password FROM users order by id desc")
+	if err != nil {
+		panic(err)
+	}
+	defer rows.Close()
+	fmt.Println("---All users from DB:")
+	countuser := 0
+	for rows.Next() {
+		err := rows.Scan(&Email, &Password)
+		if EmailNew == Email && PasswordNew == Password {
+			EmailNew = Email
+			fmt.Println("--> Email and Password Correct! WELCOME ####")
+			http.Redirect(w, r, "/home_page", http.StatusSeeOther)
+			countuser += 1
+
+		}
+
+		if err != nil {
+			panic(err)
+		}
+		fmt.Println("\n:::", Email, Password)
+	}
+
+	EmailNew = ""
+	err = rows.Err()
+	if err != nil {
+		panic(err)
+	}
+
+	if countuser <= 0 {
+		fmt.Println(w, "Password or Email incorrect, Try again!")
+		fmt.Fprintf(w, "Password or Email incorrect, Try again!")
+		return
+	}
+
+}
 func Home_page(w http.ResponseWriter, r *http.Request) {
+	fmt.Println("***Home running***")
+
 	t, err := template.ParseFiles("templates/home_page.html")
 	if err != nil {
 		fmt.Fprintf(w, err.Error())
 	}
 	t.Execute(w, "home_page")
 }
+func SessionLogout(w http.ResponseWriter, r *http.Request) {
+	session, _ := store.Get(r, "session")
+	session.Values["authenticated"] = false
+	session.Save(r, w)
+	http.Redirect(w, r, "/login", 302)
+}
+func SessionLogin(w http.ResponseWriter, r *http.Request) {
+	session, _ := store.Get(r, "session")
+	session.Values["authenticated"] = true
+	session.Values["email"] = r.FormValue("email")
+	session.Values["password"] = r.FormValue("password")
+	session.Save(r, w)
+	http.Redirect(w, r, "/welcome", 302)
+}
 func HandlerRequest() {
 	http.HandleFunc("/home_page/", Home_page)
 	http.HandleFunc("/login/", Login)
-	http.HandleFunc("/save_user/", save_user)
-	http.HandleFunc("/register/", register)
-	http.ListenAndServe(":9000", nil)
+	http.HandleFunc("/loginauth/", LoginAuth)
+	http.HandleFunc("/registerauth/", RegisterAuth)
+	http.HandleFunc("/register/", Register)
+	http.HandleFunc("/slogout/", SessionLogout)
+	http.ListenAndServe("localhost:8000", nil)
 }
--- main.go
+++ main.go
@@ -1,7 +1,16 @@
 package main
 
-import "github.com/Aktollkynn/GoProject.git/app/controllers"
+import (
+	"github.com/Aktollkynn/GoProject.git/app/controllers"
+	// "flag"
+
+)
 
 func main() {
+	
+	println("~ Welcome!")
+	println("~ http://localhost:8000/login/")
 	controllers.HandlerRequest()
+
+	
 }
